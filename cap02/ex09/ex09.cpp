f3acbdf36ea54f04b9db2b161c4e70cc60e4e973
Rafael Costacurta <rafuaero@gmail.com>
2025-01-19 09:59:22 -0300

    terminei exercicio 09

--- cap02/ex09/ex09.cpp
+++ cap02/ex09/ex09.cpp
@@ -5,5 +5,17 @@ using namespace std;
 #include "PPPheaders.h"
 
 int main (){
+    cout << "digite um numero por extenso de zero a quarto: ";
+    string resposta {};
+    cin >> resposta;
+    if (resposta == "zero") {
+        cout << "voce digitou zero = " << 0 << '\n'; 
+    } else if (resposta == "um"){
+        cout << "voce digitou um = " << 1 << '\n';
+    } else if (resposta == "dois"){
+        cout << "voce digitou dois = " << 2 << '\n';
+    } else {
+        cout << "nao reconheço esse número: " << resposta << '\n';
+    }
     return 0;
 }
