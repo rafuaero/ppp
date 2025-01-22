51de54fd69978e5e3b332c033b5503649ba8775e
Rafael Costacurta <rafuaero@gmail.com>
2025-01-21 21:23:36 -0300

    terminei exercicio 11

--- cap02/ex11/ex11.cpp
+++ cap02/ex11/ex11.cpp
@@ -23,5 +23,41 @@ int main(){
     cin >> half;
     cout << "quantos dollares vc tem? \n";
     cin >> dollar;
+
+    if (pennies <= 1){
+        cout << "vc tem " << pennies << " pennie\n";
+    } else {
+        cout << "vc tem " << pennies << " pennies\n";
+    }
+    if (nickels <= 1){
+        cout << "vc tem " << nickels << " nickel\n";
+    } else {
+        cout << "vc tem " << nickels << " nickels\n";
+    }if (dimes <= 1){
+        cout << "vc tem " << dimes << " dime\n";
+    } else {
+        cout << "vc tem " << dimes << " dimes\n";
+    }if (quarters <= 1){
+        cout << "vc tem " << quarters << " quarter\n";
+    } else {
+        cout << "vc tem " << quarters << " quarters\n";
+    }if (half <= 1){
+        cout << "vc tem " << half << " half\n";
+    } else {
+        cout << "vc tem " << half << " halfs\n";
+    }if (dollar <= 1){
+        cout << "vc tem " << dollar << " half\n";
+    } else {
+        cout << "vc tem " << dollar << " dollars\n";
+    }
+    cout << "no total voce tem: \n";
+    
+    int total_dollar {0};
+    total_dollar = (pennies + nickels*5 +  dimes*10 + quarters*25 + half*50 + dollar*100)/100;
+
+    int total_pennies {0}; 
+    total_pennies = (pennies + nickels*5 +  dimes*10 + quarters*25 + half*50 + dollar*100)%100;
+    
+    cout << total_dollar << " dolares e " << total_pennies << " pennies\n";     
     return 0;
 }
